8ce9b17a77a058203912636e7a8f142bf804b7bf
Borys <borysjair96@gmail.com>
2025-01-06 01:24:44 -0500

    Start Project Backend

---
+++ petNovaBackend/src/test/java/com/example/codesoftlution/petNovaBackend/PetNovaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codesoftlution.petNovaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetNovaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
