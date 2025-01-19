e6ce4e2bd364534dc365e9c5a69960159e4b2a66
Borys <borysjair96@gmail.com>
2025-01-18 21:02:19 -0500

    Ready Get User by Token

--- petNovaBackend/src/main/java/com/example/codesoftlution/petNovaBackend/controllers/UserController.java
+++ petNovaBackend/src/main/java/com/example/codesoftlution/petNovaBackend/controllers/UserController.java
@@ -46,10 +46,12 @@ public class UserController {
 
     @RequestMapping(value = "/userRegister", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity userRegister(@Valid @RequestBody UserModel userModel) {
-        try{
+        try {
+            log.info("START USER REGISTER");
             UserModel usuarioEncontrado = userService.findUserByEmail(userModel.getEmail(), true);
             if (usuarioEncontrado == null) {
                 userService.registerSetUser(userModel);
+                log.info("END USER REGISTER");
                 return new ResponseEntity("USUARIO CREADO", HttpStatus.CREATED);
             } else {
                 return new ResponseEntity("USUARIO NO CREADO", HttpStatus.CONFLICT);
@@ -60,13 +62,31 @@ public class UserController {
         }
     }
 
+    @RequestMapping(value = "/getUserByToken/{userToken}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getUserById(@PathVariable("userToken") String userToken) {
+        try {
+            log.info("START USER GET USER BY TOKEN: ");
+            UserModel usuaroEncontrado = userService.findUserByTokenAndActive(userToken, true);
+            if (usuaroEncontrado != null) {
+                log.info("END USER GET USER BY TOKEN: ");
+                return new ResponseEntity(usuaroEncontrado, HttpStatus.OK);
+            } else {
+                return new ResponseEntity("USUARIO NO ENCONTRADO", HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }
+
     @RequestMapping(value = "/updateUser", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity userUpdate(@Valid @RequestBody ResponseDataUserUpdate dataUserUpdate) {
         try {
+            log.info("START USER UPDATE");
             String token = request.getHeader("PnAuthorization");
 
             UserModel usuarioEncontrado = userService.findUserByTokenAndActive(token, true);
-            if(usuarioEncontrado != null) {
+            if (usuarioEncontrado != null) {
                 //Utilizo Optional.ofNullable reemplazando el if para comparar si cada atributo viene vacio
                 Optional.ofNullable(dataUserUpdate.getName()).ifPresent(usuarioEncontrado::setName);
                 Optional.ofNullable(dataUserUpdate.getUsername()).ifPresent(usuarioEncontrado::setUsername);
@@ -76,11 +96,12 @@ public class UserController {
                 Optional.ofNullable(dataUserUpdate.getLinkPerfilPhoto()).ifPresent(usuarioEncontrado::setLinkPerfilPhoto);
 
                 userService.registerSetUser(usuarioEncontrado);
+                log.info("END USER UPDATE");
                 return new ResponseEntity("USUARIO ACTUALIZADO", HttpStatus.OK);
-            }else {
+            } else {
                 return new ResponseEntity("USUARIO NO ENCONTRADO", HttpStatus.CONFLICT);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
@@ -88,12 +109,14 @@ public class UserController {
     @RequestMapping(value = "/getUsers", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity getUsers() {
         try {
+            log.info("START GETUSERS");
             List<UserModel> userModelList = userService.getUsers();
 
             ListUserResponse listUserResponse = new ListUserResponse();
             listUserResponse.setUserModels(userModelList);
             listUserResponse.setMessage("Lista de Usuarios");
 
+            log.info("END GETUSERS");
             return new ResponseEntity<>(listUserResponse, HttpStatus.OK);
 
         } catch (Exception e) {
@@ -103,18 +126,20 @@ public class UserController {
 
     @RequestMapping(value = "/deleteUser", method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity deleteUser() {
-        try{
+        try {
+            log.info("START DELETEUSER");
             String token = request.getHeader("PnAuthorization");
             UserModel usuarioEncontrado = userService.findUserByTokenAndActive(token, true);
-            if(usuarioEncontrado != null) {
+            if (usuarioEncontrado != null) {
                 usuarioEncontrado.setActive(false);
                 userService.registerSetUser(usuarioEncontrado);
 
+                log.info("END DELETEUSER");
                 return new ResponseEntity("USUARIO ELIMINADO", HttpStatus.OK);
-            }else{
+            } else {
                 return new ResponseEntity("USUARIO NO ENCONTRADO", HttpStatus.CONFLICT);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
@@ -122,6 +147,7 @@ public class UserController {
     @RequestMapping(value = "/pnCifrado/{token}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public String pnCifrado(@PathVariable("token") String token) {
         try {
+            log.info("START PNCIFRADO");
             IvParameterSpec iv = new IvParameterSpec(PN_INIT_VECTOR.getBytes(STANDARDCHARSETS_UFT_8));
             SecretKeySpec secretKeySpec = new SecretKeySpec(PN_AES_KEY.getBytes(STANDARDCHARSETS_UFT_8), ALGORITHM_AES);
             Cipher cipher;
@@ -130,6 +156,7 @@ public class UserController {
 
             byte[] encrypted = cipher.doFinal(token.getBytes());
 
+            log.info("END PNCIFRADO");
             return Base64.getUrlEncoder()
                     .encodeToString(encrypted);
 
@@ -143,12 +170,14 @@ public class UserController {
     @RequestMapping(value = "/pnDescifrado/{tokenCifrado}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public String pnDescifrado(@PathVariable("tokenCifrado") String token) {
         try {
+            log.info("START PNDESCIFRADO");
             IvParameterSpec iv = new IvParameterSpec(PN_INIT_VECTOR.getBytes(STANDARDCHARSETS_UFT_8));
             SecretKeySpec secretKeySpec = new SecretKeySpec(PN_AES_KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM_AES);
             Cipher cipher = Cipher.getInstance(TRANSFORMATION_AES);
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, iv);
 
             byte[] desencriptado = cipher.doFinal(Base64.getUrlDecoder().decode(token));
+            log.info("END PNDESCIFRADO");
             return new String(desencriptado);
         } catch (InvalidAlgorithmParameterException | UnsupportedEncodingException | NoSuchPaddingException |
                  IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException | InvalidKeyException e) {
