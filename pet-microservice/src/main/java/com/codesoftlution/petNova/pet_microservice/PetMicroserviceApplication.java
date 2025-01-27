d3c1a24be8f11d910215488d409e328179f8a200
Borys <borysjair96@gmail.com>
2025-01-26 23:40:53 -0500

    Lista la configuración del config Server para obtenerl las properties desde un repositorio aparte

---
+++ pet-microservice/src/main/java/com/codesoftlution/petNova/pet_microservice/PetMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petNova.pet_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMicroserviceApplication.class, args);
+	}
+
+}
---
+++ pet-microservice/src/test/java/com/codesoftlution/petNova/pet_microservice/PetMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petNova.pet_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
